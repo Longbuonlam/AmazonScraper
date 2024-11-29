972380a36da962ac0b1b27bdfc5b5cf036fc37d0
Long <mac@MacBookPro.lan>
2024-11-29 19:57:38 +0700

    init project again

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # AmazonScraper
 
-1. npm install crawlbase
-2. node index.js
+1. sign up and get token from https://crawlbase.com/
+2. create .env file in project and paste TOKEN='your token'
+2. npm install crawlbase
+3. node index.js
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const https = require('https');
 const fs = require('fs');
+require('dotenv').config();
 
 // URL of the Amazon product page
 const url = encodeURIComponent('https://www.amazon.com/dp/B0B7CBZZ16?th=1');     
 const options = {
   hostname: 'api.crawlbase.com',
-  path: '/?token=FHVib6rqJcvAYbGpVHEpMQ&scraper=amazon-product-details&url=' + url
+  path: '/?token=' + process.env.TOKEN + '&scraper=amazon-product-details&url=' + url
 };
 
 https.request(options, (response) => {
